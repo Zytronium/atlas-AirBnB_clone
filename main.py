346cd79d7d681ad6c132a1adaba5310f4438a31b
Zytronium <dastelljes@outlook.com>
2024-09-25 15:36:34 -0500

    added cmd line interface

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 #!/bin/python3
+from cmd import Cmd
 
+class MyCmd(Cmd):
+    def __init__(self):
+        super().__init__()
+        self.intro = ('Welcome to AirBnB Clone Console! Type '
+                       '"help" or "?" for a list of commands.')
+        self.prompt = '> '
+
+    @staticmethod
+    def do_exit(self):
+        """Exit the program."""
+        return True
+
+
+    @staticmethod
+    def do_EOF(self):
+        """EOF signal (usually ctl+d) will run this to exit the program."""
+        print()
+        return True
+
+if __name__ == '__main__':
+    MyCmd().cmdloop()
