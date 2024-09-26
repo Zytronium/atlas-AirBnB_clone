5182ed1076203d73ec4be37187c0f74d33743301
Clay <rpctropic13@gmail.com>
2024-09-26 21:51:30 +0000

    changed init

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,17 @@ class BaseModel:
 
         format = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for name, value in kwargs.items():
                 if name == 'created_at' or name == 'updated_at':
                     self.__dict__[value] = datetime.strptime(value, format)
                 else:
                     self.__dict__[name] = value
+
             
+        
 
     def __str__(self):
         """Print the instance"""
@@ -36,7 +38,7 @@ class BaseModel:
         """Return a dictionary containing the instance"""
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
-        dict["created_at"] = self.created_at
-        dict["updated_at"] = self.updated_at
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
         return dict
 
