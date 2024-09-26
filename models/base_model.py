c2c81b5498c46e6035ccce557addbbc6ee56a847
Clay <rpctropic13@gmail.com>
2024-09-26 20:38:55 +0000

    storage module

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""The base_model module."""
 import uuid
+import models
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """Base atributes for each instance"""
@@ -20,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
 
     def __str__(self):
@@ -29,6 +32,7 @@ class BaseModel:
     def save(self):
         """Save the instance"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def to_dict(self):
         """Return a dictionary containing the instance"""
