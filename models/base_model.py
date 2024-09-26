b609d261092b757256a583ea80749c1c4c96e519
Clay <rpctropic13@gmail.com>
2024-09-26 21:24:14 +0000

    check

--- models/base_model.py
+++ models/base_model.py
@@ -12,20 +12,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize the instance"""
 
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().strftime(time_format)
+        self.updated_at = datetime.now().strftime(time_format)
         if kwargs:
-            for name, value in kwargs.items():
-                if name == "__class__":
-                    self.__dict__[name] = value
-                elif name in ("created_at", "updated_at"):
-                    self.__dict__[name] = datetime.now()
-                else:
-                    self.__dict__[name] = value
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-
+            for attr_name, attr_value in kwargs.items():
+                if attr_name == 'created_at' or attr_name == 'updated_at':
+                    continue
+                setattr(self, attr_name, attr_value)
 
     def __str__(self):
         """Print the instance"""
@@ -35,12 +30,11 @@ class BaseModel:
         """Save the instance"""
         self.updated_at = datetime.today()
         
-
     def to_dict(self):
         """Return a dictionary containing the instance"""
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
-        dict["created_at"] = self.created_at.isoformat()
-        dict["updated_at"] = self.updated_at.isoformat()
+        dict["created_at"] = self.created_at
+        dict["updated_at"] = self.updated_at
         return dict
 
