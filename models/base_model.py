3dbd7de7afe4fbc7f4576e4ddd99c84452f8c322
Clay <rpctropic13@gmail.com>
2024-09-26 17:45:01 +0000

    promising base

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 import uuid
+from datetime import datetime
+
 
 class BaseModel:
     """Base atributes for each instance"""
     def __init__(self, *args, **kwargs):
         """Initialize the instance"""
+
         if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    self.__dict__[key] = value
+            for name, value in kwargs.items():
+                if name == "__class__":
+                    self.__dict__[name] = value
+                elif name in ("created_at", "updated_at"):
+                    self.__dict__[name] = datetime.now()
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[name] = value
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = self.id
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """Print the instance"""
-        return "[{}] ({}) {}".format(
-          self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Save the instance"""
-        self.updated_at = self.id
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Return a dictionary containing the instance"""
-      dict = {}
-      for key, value in self.__dict__.items():
-        if key != "id" and key != "created_at" and key != "updated_at":
-          dict[key] = value
-      return dict
+        dict = self.__dict__.copy()
+        dict["__class__"] = self.__class__.__name__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        return dict
+
