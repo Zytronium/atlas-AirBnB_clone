ab037973f6d6216ac56b68e251464bb978113bb0
Clay <rpctropic13@gmail.com>
2024-09-27 23:58:26 +0000

    initializated self variables in the else statement

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,6 @@ class BaseModel:
         """initialize the instance"""
  
         format = '%Y-%m-%dT%H:%M:%S.%f'
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
         if kwargs:
             for name, value in kwargs.items():
                 if name == 'created_at' or name == 'updated_at':
@@ -22,6 +19,9 @@ class BaseModel:
                 else:
                     self.__dict__[name] = value
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
             
     def __str__(self):
