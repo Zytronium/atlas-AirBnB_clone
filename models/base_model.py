5248cfa2e0fe268e0dc3c33682cbf2fa495f4abc
Clay <rpctropic13@gmail.com>
2024-09-27 22:52:50 +0000

    updated save

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """The base_model module."""
-import uuid
 import models
+import uuid
 from datetime import datetime
-from .engine import file_storage
 
 
 
 class BaseModel:
-    """Base atributes for each instance"""
+    """base atributes for each instance"""
     def __init__(self, *args, **kwargs):
-        """Initialize the instance"""
-
+        """initialize the instance"""
+ 
         format = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
@@ -22,17 +21,20 @@ class BaseModel:
                     self.__dict__[value] = datetime.strptime(value, format)
                 else:
                     self.__dict__[name] = value
-
+        else:
+            models.storage.new(self)
+            
     def __str__(self):
-        """Print the instance"""
+        """print the instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Save the instance"""
+        """save the instance"""
         self.updated_at = datetime.today()
+        models.storage.save()
         
     def to_dict(self):
-        """Return a dictionary containing the instance"""
+        """return a dictionary containing the instance"""
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
         dict["created_at"] = self.created_at.isoformat()
