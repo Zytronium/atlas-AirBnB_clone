7085ae15d451d503bf9700c83896d4df9517ec49
Clay <rpctropic13@gmail.com>
2024-10-02 05:54:13 +0000

    test

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""classs for testing Review"""
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """testing for the __init__"""
+
+
+    def test_init(self):
+        """__init__"""
+
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
