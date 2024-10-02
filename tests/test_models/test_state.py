1d076b60c27e81dccafcc56a8645de0559221aee
Clay <rpctropic13@gmail.com>
2024-10-02 05:39:21 +0000

    test

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class for testing state"""
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """class for testing state"""
+    
+    def setUp(self):  
+        """setup for testing"""
+        
+        self.state = State()
+        self.state.name = 'Texas'
+  
+    def test__init__(self):
+        """test __init__"""
+      
+        self.assertEqual(self.state.name, 'Texas')
