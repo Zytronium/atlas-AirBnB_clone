aa1497384d91b2ce8dcb222fc440a5c936017e46
Clay <rpctropic13@gmail.com>
2024-10-02 04:38:07 +0000

    test

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -1,55 +1,46 @@
-#!/usr/bin/python3
 import unittest
 import datetime
-from uuid import uuid4
+import uuid
 from models.base_model import BaseModel
 
-"""test module for the BaseModel class"""
-
-
 class TestBaseModel(unittest.TestCase):
-    """test class for the BaseModel class"""
 
-        
     def test_to_dict(self):
         """test to_dict method"""
         my_model = BaseModel()
-        dict = my_model.to_dict()
-        self.assertEqual(dict['__class__'], 'BaseModel')
-        self.assertEqual(dict['id' ], str(uuid4()))
-        self.assertEqual(dict['created_at'],
-                         datetime.datetime.today().isoformat())
-        self.assertEqual(dict['updated_at'],
-                         datetime.datetime.today().isoformat())
+        dict_rep = my_model.to_dict()
+        self.assertEqual(dict_rep['__class__'], 'BaseModel')
+        self.assertEqual(dict_rep['id'], my_model.id)
+        self.assertEqual(dict_rep['created_at'],
+                        my_model.created_at.isoformat())
+        self.assertEqual(dict_rep['updated_at'],
+                        my_model.updated_at.isoformat())
 
-    
     def test_save(self):
         """test save method"""
         base = BaseModel()
+        old_updated_at = base.updated_at
         base.save()
-        self.assertEqual(base.updated_at, datetime.datetime.today())
-        
-        
+        self.assertNotEqual(base.updated_at, old_updated_at)
 
     def test__str__(self):
         """test __str__ method"""
         base = BaseModel()
         self.assertEqual(str(base), "[BaseModel] ({}) {}".format(
-            base.id, base.__dict__))
+                base.id, base.__dict__))
 
     def test_kwarg_init(self):
         """test kwargs that init uses"""
-        self.date = "1971-01-01T00:00:00.000000"
-        self.split = {
-            'user': '3489', 'created_at': self.date, 'updated_at': self.date}
-        base = BaseModel(**self.split)
+        date_str = "1971-01-01T00:00:00.000000"
+        kwargs = {
+            'id': '3489', 
+            'created_at': date_str, 
+            'updated_at': date_str
+            }
+        base = BaseModel(**kwargs)
         self.assertEqual(base.id, '3489')
-        self.assertEqual(base.created_at.isoformat(
-            timespec='seconds'), self.date)
-        
-        
-
-
+        self.assertEqual(base.created_at.isoformat(), date_str)
+        self.assertEqual(base.updated_at.isoformat(), date_str)
 
     def test_base_id(self):
         """test the id attribute"""
@@ -59,22 +50,14 @@ class TestBaseModel(unittest.TestCase):
 
     def test_initializaton(self):
         """test the __init__ method"""
-        
         my_model = BaseModel()
-        self.assertEqual(my_model.id, str(uuid.uuid4()))
+        self.assertIsInstance(my_model.id, str)
         self.assertEqual(my_model.__class__.__name__, "BaseModel")
         self.assertIsInstance(my_model.created_at, datetime.datetime)
         self.assertIsInstance(my_model.updated_at, datetime.datetime)
-        self.assertGreater(my_model.created_at, my_model.updated_at)
-        self.assertNotEqual(my_model.created_at, my_model.updated_at)
+        self.assertLessEqual(my_model.created_at, my_model.updated_at)
 
 
 if __name__ == "__main__":
-    unittest.main()
+        unittest.main()
     
-    
-    
-    
-
-  
-  
